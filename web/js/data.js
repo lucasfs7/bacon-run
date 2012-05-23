86e357e28cbcbb9595577bec530f433bc6d0285e
Wilton <wiltongarcia@hotmail.com>
2012-05-23 15:43:44 -0300

    data file

---
+++ web/js/data.js
@@ -0,0 +1,11 @@
+var game_data = [
+    {
+        enemies:[
+            {
+                sprite:"", // caminho da imagem
+                hp:100, // inteiro
+                velocity:2 // inteiro de 1 a 5
+            }
+        ]
+    }
+]
