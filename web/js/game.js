06008f115b63d745c7f9cfb42a871fcf19f6de13
Lucas Souza <lucas@silika.com.br>
2012-05-23 17:52:51 -0300

    sprite porco / morte do porco

--- web/js/game.js
+++ web/js/game.js
@@ -9,12 +9,36 @@
         game.level = 0;
         
         //player
-        var player = game.spr("img/zero.png", 45, 65, 9);
-        player.position(100, 245, 1).speed(3);
+        var player = game.spr("img/pig.png", 105, 80, 7);
+        player.position(100, 235, 1).speed(3);
         player.animation(1);
+        player.points = 0;
         player.jump = false;
-        player.espada = false;
-        player.soco = false;
+        player.shield = false;
+        player.shuriken = { sprite: "img/shuriken.png", speed: 3, damage: 10 };
+        player.alive = true;
+        player.die = function()
+        {
+            if(!player.shield)
+            {
+                player.animation(0);
+                player.callback(function()
+                {
+                    player.alive = false;
+                    player.frame(7);
+                    bg.off();
+                    inimigo.callback(function()
+                    {
+                        game.off();
+                    }, 2);
+                }, 1);
+            }
+            else
+            {
+                inimigo.animation(3);
+                player.shield = false;
+            }
+        };
         
         //bg
         var bg = game.bg("img/bg.jpg", 3, "W", { x:0,y:0 });
@@ -22,37 +46,25 @@
         
         //inimigo
         var inimigo = game.spr("img/zero.png", 45, 65, 9);
-        inimigo.position(global.innerWidth - 45, 245, 1).speed(3);
+        inimigo.position(global.innerWidth - 45, 250, 1).speed(3);
         inimigo.animation(2);
         inimigo.jump = false;
-        
-        inimigo.hit(player, function()
-        {
-            if(!player.espada && !player.soco)
-            {
-                game.off();
-            }
-            else
-            {
-                inimigo.animation(3);
-            }
-        });
+        inimigo.hit(player, player.die);
         
         var keyControll = function(e)
         {
             var keyCode = e.keyCode;
             
-            if(keyCode == 88)
+            if(keyCode == 88 && player.alive)
             {
                 //x
-                player.espada = true;
-                player.animation(0);
-            }
-            else if(keyCode == 90)
-            {
-                //z
-                player.soco = true;
-                player.animation(3);
+                player.shield = true;
+                bg.off();
+                player.animation(2);
+                player.callback(function()
+                {
+                    bg.on();
+                }, 1)
             }
             
             if(keyCode == 32)
